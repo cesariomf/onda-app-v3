8bf46634220d5ed3cc8cc2ef3ff5ce90d6052761
cesariomf <cesariomf@mac.com>
2026-04-05 22:31:24 -0300

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Onda from './Onda.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Onda />
+  </React.StrictMode>,
+)
